abae3afe49c011d2fe771fde1200a78f11ae1371
DanilProV-ctrl <ProVDanil2019@yandex.ru>
2020-02-16 13:36:59 +1000

    gg)

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+import random
+codes = [31, 32, 33, 34, 35, 36, 37, 91, 98, 94, 38, 45, 65, 66]
+
+print()
+a = ord('A')
+for i in range(a, a + 32):
+        time.sleep(0.3)
+        print(f'{chr(155)}{random.choice(codes)};1m{chr(i)}', end=' ', flush=True)
+def gg(a):
+        return
+print('\x1b[0m', gg(a, fun))
